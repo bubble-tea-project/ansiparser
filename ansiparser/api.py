466d569e236b3210738145001b5de10a594402ac
vHrqO <120627599+vHrqO@users.noreply.github.com>
2024-10-10 00:22:32 +0800

    Update comments

--- ansiparser/api.py
+++ ansiparser/api.py
@@ -11,13 +11,13 @@ from . import screen_parser
 
 
 def new_screen(height=24,width=80) -> screen_parser.ScreenParser:
-    """Initialize new  parser for screen """
+    """Initialize the ScreenParser for a new screen."""
     
     return screen_parser.ScreenParser(screen_height=height,screen_width=width)
 
 
 def from_screen(parsed_screen: deque) -> screen_parser.ScreenParser:
-    """Initialize from an existing parsed screen."""
+    """Initialize the ScreenParser from an existing parsed screen."""
 
     screen_parser_class = screen_parser.ScreenParser()
     screen_parser_class.from_parsed_screen(parsed_screen)
