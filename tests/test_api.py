0b0a535f36955e3f94a48332d424e831c15c26b4
vHrqO <120627599+vHrqO@users.noreply.github.com>
2024-11-05 21:33:06 +0800

    test: refactor unit tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,44 +1,48 @@
 import pytest
-from ansiparser.api import new_screen, from_screen
-from ansiparser.screen_parser import ScreenParser
 
+from ansiparser.api import from_screen, new_screen
+from ansiparser.screen_parser import ScreenParser
 from ansiparser.structures import InterConverted, SgrAttributes
 
 
-class TestAPI:
+class TestNewScreen:
 
-    def test_new_screen_default(self):
-        
+    def test_default(self):
+
         ansip_screen = new_screen()
+
         assert isinstance(ansip_screen, ScreenParser)
         assert ansip_screen.screen_height == 24
         assert ansip_screen.screen_width == 80
 
-    def test_new_screen_custom(self):
-        
+    def test_custom(self):
+
         ansip_screen = new_screen(height=30, width=100)
 
         assert isinstance(ansip_screen, ScreenParser)
         assert ansip_screen.screen_height == 30
         assert ansip_screen.screen_width == 100
 
-    def test_from_screen(self):
 
-        inter_converted = InterConverted()
-        inter_converted.text = ["t", "e", "s", "t"]
-        inter_converted.styles = [SgrAttributes() for _ in range(4)]
+class TestfromScreen:
 
-        parsed_screen = [inter_converted , inter_converted]
-        
-  
-        ansip_screen = from_screen(parsed_screen)
+    def test_normal(self):
+
+        normal_parsed_screen = []
+
+        for line in range(2):
+            inter_converted = InterConverted()
+            inter_converted.text = ["t", "e", "s", "t"]
+            inter_converted.styles = [SgrAttributes() for char in range(4)]
+
+            normal_parsed_screen.append(inter_converted)
+
+        ansip_screen = from_screen(normal_parsed_screen)
         assert isinstance(ansip_screen, ScreenParser)
-        # assert ansip_screen.get_parsed_screen() == parsed_screen
 
-    def test_from_screen_invalid(self):
-        
+    def test_invalid(self):
+
         invalid_parsed_screen = None
-        
+
         with pytest.raises(TypeError):
             from_screen(invalid_parsed_screen)
-
