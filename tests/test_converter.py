5bb9ef5c03972dce007079ee6ec7c0dae3ff23ad
vHrqO <120627599+vHrqO@users.noreply.github.com>
2024-11-05 22:30:11 +0800

    test: refactor unit tests

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,94 +1,71 @@
 import pytest
-from bs4 import BeautifulSoup
+
 from ansiparser.converter import sgr_attributes_to_css, to_html, to_string
-from ansiparser.structures import InterConverted, SgrAttributes, WCharPH
+from ansiparser.structures import InterConverted, SgrAttributes
 
 
 def test_sgr_attributes_to_css():
-    # Prepare test SGR attributes
+
     sgr_attributes = SgrAttributes()
-    sgr_attributes.style = {"sgr_1"}
-    sgr_attributes.foreground ="sgr_30"
-    sgr_attributes.background ="sgr_40"
-    
-    css_class = sgr_attributes_to_css(sgr_attributes)
+    sgr_attributes.style = {'bold'}
+    sgr_attributes.foreground = 'fg_blue'
+    sgr_attributes.background = 'bg_white'
 
-    assert css_class == "sgr_1 sgr_30 sgr_40"
+    assert sgr_attributes_to_css(sgr_attributes) == "bold fg_blue bg_white"
 
 
-def test_to_html_valid():
-    # Prepare test InterConverted object
+@pytest.fixture()
+def valid_inter_converted():
+
     inter_converted = InterConverted()
-    inter_converted.text = ["A", "B"]
+    inter_converted.text = ["R", " ", "G"]
 
-    sgr_attributes = SgrAttributes()
-    sgr_attributes.style = {"sgr_1", "sgr_2"}
-    sgr_attributes.foreground ="sgr_30"
-    sgr_attributes.background ="sgr_40"
+    attribute_red = SgrAttributes()
+    attribute_red.background = 'fg_red'
 
-    sgr_attributes_1 = SgrAttributes()
-    sgr_attributes_1.style = {"sgr_1"}
-    sgr_attributes_1.foreground ="sgr_30"
-    sgr_attributes_1.background ="sgr_40"
+    attribute_green = SgrAttributes()
+    attribute_green.foreground = 'fg_green'
 
-    inter_converted.styles = [sgr_attributes,sgr_attributes_1]
+    inter_converted.styles = [attribute_red, attribute_red, attribute_green]
+
+    return inter_converted
 
 
-    result = to_html(inter_converted)
+@pytest.fixture()
+def invalid_inter_converted():
 
-    assert result.name == "div"
-    assert len(result.select("span")) == 2
-
-    assert result.select("span")[0].string == "A"
-    assert "sgr_30" in result.select("span")[0]["class"]
-
-
-
-
-def test_to_html_invalid():
-    # Prepare an invalid InterConverted object
     inter_converted = InterConverted()
-    inter_converted.text = ["A", "B"]
+    inter_converted.text = ["R", " ", "G"]
 
     sgr_attributes = SgrAttributes()
-    sgr_attributes.style = {"sgr_1", "sgr_2"}
-    sgr_attributes.foreground ="sgr_30"
-    sgr_attributes.background ="sgr_40"
-
-   
-    inter_converted.styles = [sgr_attributes]
-
-    with pytest.raises(ValueError):
-        to_html(inter_converted)
-
-
-def test_to_string_valid():
-    # Prepare test InterConverted object
-    inter_converted = InterConverted()
-    inter_converted.text = ["A", "B"]
-    
-    sgr_attributes = SgrAttributes()
-    sgr_attributes.style = {"sgr_1", "sgr_2"}
-    sgr_attributes.foreground ="sgr_30"
-    sgr_attributes.background ="sgr_40"
-
-    inter_converted.styles = [sgr_attributes,sgr_attributes]
-
-    result = to_string(inter_converted)
-    assert result == "AB"
-
-
-def test_to_string_invalid():
-    # Prepare an invalid InterConverted object
-    inter_converted = InterConverted()
-    inter_converted.text = ["A", "B"]
-
-    sgr_attributes = SgrAttributes()
-    sgr_attributes.style = {"sgr_1", "sgr_2"}
-    sgr_attributes.foreground ="sgr_30"
-    sgr_attributes.background ="sgr_40"
 
     inter_converted.styles = [sgr_attributes]
 
+    return inter_converted
+
+
+def test_to_html_valid(valid_inter_converted: InterConverted):
+
+    result = str(to_html(valid_inter_converted))
+    answer = '<div class="line"><span class="fg_red">R </span><span class="fg_green">G</span></div>'
+
+    assert result == answer
+
+
+def test_to_html_invalid(invalid_inter_converted: InterConverted):
+
     with pytest.raises(ValueError):
-        to_string(inter_converted)
+        to_html(invalid_inter_converted)
+
+
+def test_to_string_valid(valid_inter_converted: InterConverted):
+
+    result = to_string(valid_inter_converted)
+
+    assert result == 'R G'
+
+
+def test_to_string_invalid(invalid_inter_converted: InterConverted):
+
+    with pytest.raises(ValueError):
+        to_string(invalid_inter_converted)
